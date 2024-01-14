56c398d7ea7d4faa0764e40cb69af38aa845e2d8
shiraznazir <shiraznazir786@gmail.com>
2024-01-15 02:04:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.port, () => {
+  console.log(`App listening on port ${port}`);
+});
